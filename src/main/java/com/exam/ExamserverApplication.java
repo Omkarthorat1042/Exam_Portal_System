ce5f6a588153b59e28d615b7adaf5c39270a4f11
Omkarthorat1042 <omkarthorat1042@gmail.com>
2025-01-04 21:47:30 +0530

    Created repositories and also schema for databases

---
+++ src/main/java/com/exam/ExamserverApplication.java
@@ -0,0 +1,60 @@
+package com.exam;
+
+import com.exam.model.Role;
+import com.exam.model.User;
+import com.exam.model.UserRole;
+import com.exam.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootApplication
+public class ExamserverApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserService userService;
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamserverApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("stating code");
+
+		User user = new User();
+
+		user.setLastName("Omkar");
+		user.setLastName("Thorat");
+		user.setUsername("omkarthorat");
+		user.setPassword("Thorat@18");
+		user.setEmail("omkar@gmail.com");
+		user.setProfile("User.png");
+
+		Role role = new Role();
+		role.setRoleName("ADMIN");
+		role.setRoleId(44L);
+
+		Set<UserRole> userRoleSet = new HashSet<>();
+		UserRole userRole = new UserRole();
+		userRole.setRole(role);
+		userRole.setUser(user);
+
+
+
+
+		userRoleSet.add(userRole);
+		User user1 = this.userService.createUser(user, userRoleSet);
+
+		System.out.println(user1.getUsername());
+
+
+
+	}
+}
---
+++ src/main/java/com/exam/service/impl/UserSeriveImpl.java
@@ -0,0 +1,52 @@
+package com.exam.service.impl;
+
+import com.exam.model.User;
+import com.exam.model.UserRole;
+import com.exam.repo.RoleRepository;
+import com.exam.repo.UserRepository;
+import com.exam.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLOutput;
+import java.util.Set;
+
+@Service
+public class UserSeriveImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+//creating user
+    @Override
+    public User createUser(User user, Set<UserRole> userRoles) throws Exception{
+
+       User local = this.userRepository.findByUsername(user.getUsername());
+
+       if(local != null)
+       {
+           System.out.println("User is already there");
+
+            throw new Exception("User already exists !!");
+
+       }else {
+
+           //create user
+           for(UserRole ur: userRoles){
+
+               roleRepository.save(ur.getRole());
+
+           }
+           user.getUserRole().addAll(userRoles);
+           local = this.userRepository.save(user);
+
+
+       }
+
+
+        return local;
+    }
+}
